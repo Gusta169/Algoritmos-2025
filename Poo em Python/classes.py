b58642036354b12783b1762f965cba29ab1e8594
Gusta169 <gustavoliveirablu1298@gmail.com>
2025-11-11 22:32:41 -0300

    Update classes.py

--- Poo em Python/classes.py
+++ Poo em Python/classes.py
@@ -26,3 +26,28 @@ ventilador_1.velocidade_atual = 0
 print(ventilador_1.velocidade_atual)
 aumentarVelocidade(ventilador_1)
 print(ventilador_1.velocidade_atual)
+
+class Televisao:
+    def __init__(self):
+        self.status = bool
+        self.canal = 0
+        self.volume = 0
+def Ligar(tv):
+    tv.status = True
+    tv.canal = 1
+
+def desligar(tv):
+    tv.status = False
+    tv.canal = 0
+
+def aumentar_canal(tv):
+    if(tv.canal <= 5):
+        tv.canal += 1
+def mudarCanal(tv, canal):
+    if(canal <= 5):
+        tv.canal = canal
+
+tv = Televisao()
+tv.status = Ligar(tv)
+tv.canal = 3 
+tv.volume = 50
