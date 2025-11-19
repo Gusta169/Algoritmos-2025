d8cae87fb63785f76279cd256131617085ca62b8
Gusta169 <gustavoliveirablu1298@gmail.com>
2025-11-18 22:17:08 -0300

    Aula de Estrutura de Dados
    Apresentados matrizes, formas de utilizar classes, listas, tuplas, conjuntos e dicionarios(dict).

---
+++ Estrutura de Dados.py
@@ -0,0 +1,102 @@
+# #Matriz
+
+# linhas = 4 
+# colunas = 3
+# matriz = [0] * linhas
+# for i in range( 0, linhas):
+#     matriz[i] = 0 * colunas
+# print(matriz)
+
+# matriz[1][2] = 5
+
+# print(matriz)
+
+# #Classes
+
+# class Disciplina:
+#     def __init__(self):
+#         self.id = 0
+#         self.nome = ""
+#         self.ch = ""
+#         self.professor = ""
+
+# #Listas 
+# lista = []
+# print(lista)
+
+
+# lista.append(5) #append - insere no fim
+# lista.append("Aula")
+# lista.append(80.2)
+
+# print(lista)
+# lista.insert(2, "oi") #insere no indice iformado
+# print(lista)
+
+# lista.pop(2) #exclui o valor do indice
+# print(lista)
+
+# lista.remove(80.2) #exclui até encontrar oque foi colocado
+
+# print(lista)
+
+#-----------------------------------------------------------
+
+#exercicio
+
+# lista = []
+# for i in range(0, 5):
+#     usuario = input("Digite os nomes: ")
+#     lista.append(usuario)
+
+# for i in range(0, len((usuario))):
+#     usuario = input("Digite o nome que voce deseja remover: ")
+#     lista.remove(usuario)
+#     break
+# print("--------Lista Final-------")
+# for i in range(0, len(lista)):
+#     print(lista[i])
+
+#Tuplas
+#Tuplas são imutaveis
+# tuplas = ("Ayslan", 40, 8,79, False)
+# print(tuplas[2])
+# print(tuplas)
+
+# nome = input("Informe o nome do Produto: ")
+# preço = float(input("Informe o preço do produto"))
+# estoque = int(input("Informe q quantidade que contem no estoque"))
+
+# produto = (nome, preço, estoque)
+
+# for i in range(0, len(produto)):
+#     print(produto[i])
+#     tamanho = len(produto)
+
+# print(f"tamanho da tupla {tamanho}")
+
+#Conjuntos
+
+# conjunto = set()
+# conjunto.add(1)
+# conjunto.add(4)
+# conjunto.add(7)
+# conjunto.add(9)
+# conjunto.add(1)
+
+# turma_a = {"Ana", "Beto", "Carlos", "Duda"}
+# turmba_b = {"Carlos", "Edu", "Fernanda", "Ana"}
+
+#Dicionario (dict)
+#chave : valor
+#Exemplo:
+pessoa = {
+    "nome": "Alberto",
+    "idade": 35,
+    "filhos": {"Pedro", "Maria"},
+    "formacao": ["Informatica", "Egenharia de Software"],
+    "profissao": {
+        "empresa": "Google",
+        "cargo": "Desenvolvedor"
+    }
+}
